3e994462cc6e2c9a61b1fd712f03c14079cf5109
DanielGroelle <DanielGroelle@protonmail.com>
2023-12-13 21:30:52 -0500

    Prevent pasting and right clicking the race input textbox

--- src/app/race/TextInputComponent.tsx
+++ src/app/race/TextInputComponent.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import React, {useState, FormEvent} from "react";
+import React, {useState, ChangeEvent, ClipboardEvent, MouseEvent} from "react";
 
 export default function TextInputComponent() {
   const [userInput, setUserInput] = useState("");
-  const handleChange = (event: FormEvent<HTMLTextAreaElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(event.currentTarget.value);
     setUserInput(event.currentTarget.value);
   };
 
+  const handlePaste = (event: ClipboardEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+  };
+
+  const handleContextMenu = (event: MouseEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+  }
+
   return (
-    <textarea className="text-black resize-none" onChange={handleChange}></textarea>
+    <textarea className="text-black resize-none" onChange={handleChange} onPaste={handlePaste} onContextMenu={handleContextMenu}></textarea>
   );
 }
