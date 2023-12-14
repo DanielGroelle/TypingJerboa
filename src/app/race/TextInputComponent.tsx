f5939ea144f7a106615b8c0e1c78f7c35105a0a0
DanielGroelle <DanielGroelle@protonmail.com>
2023-12-14 03:39:06 -0500

    Implement correct/incorrect userInput styling

--- src/app/race/TextInputComponent.tsx
+++ src/app/race/TextInputComponent.tsx
@@ -1,23 +1,47 @@
 "use client";
 
 import React, {useState, ChangeEvent, ClipboardEvent, MouseEvent} from "react";
+import "../globals.css";
 
-export default function TextInputComponent() {
-  const [userInput, setUserInput] = useState("");
+export default function TextInputComponent({raceParagraphArray}: {raceParagraphArray: string[]}) {
+  const [correctCharacters, setCorrectCharacters] = useState<string[]>([]);
+
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    console.log(event.currentTarget.value);
-    setUserInput(event.currentTarget.value);
+    const userInput = event.currentTarget.value;
+
+    const newCorrectCharacters = raceParagraphArray.reduce((accumulator: string[], char, i)=>{
+      if (userInput[i] === undefined) {
+        return accumulator.concat("empty");
+      }
+      if (char !== userInput[i]) {
+        return accumulator.concat("incorrect");
+      }
+      return accumulator.concat("correct");
+    }, []);
+
+    setCorrectCharacters(newCorrectCharacters);
   };
 
   const handlePaste = (event: ClipboardEvent<HTMLTextAreaElement>) => {
     event.preventDefault();
   };
 
-  const handleContextMenu = (event: MouseEvent<HTMLTextAreaElement>) => {
+  const handleTextAreaContextMenu = (event: MouseEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+  }
+
+  const handleParagraphContextMenu = (event: MouseEvent<HTMLParagraphElement>) => {
     event.preventDefault();
   }
 
   return (
-    <textarea className="text-black resize-none" onChange={handleChange} onPaste={handlePaste} onContextMenu={handleContextMenu}></textarea>
+    <div>
+      <div className="border-solid border-white border select-none" onContextMenu={handleParagraphContextMenu}>
+        {raceParagraphArray.map((character, i)=>{
+          return <span className={correctCharacters[i] ?? "empty"} key={i}>{character}</span>
+        })}
+      </div>
+      <textarea className="text-black resize-none" onChange={handleChange} onPaste={handlePaste} onContextMenu={handleTextAreaContextMenu}></textarea>
+    </div>
   );
 }
--- src/app/race/client-page.tsx
+++ src/app/race/client-page.tsx
@@ -9,6 +9,8 @@ export default function ClientRace() {
   const [raceParagraph, setRaceParagraph] = useState("Text");
   const [startTime, setStartTime] = useState(null as Date | null);
 
+  const raceParagraphArray = [...raceParagraph];
+
   return (
     <div>
       <div className="flex flex-col m-10">
@@ -16,8 +18,7 @@ export default function ClientRace() {
           <ScriptSelectionComponent setRaceParagraph={setRaceParagraph} setStartTime={setStartTime} />
           <TimerComponent startTime={startTime} />
         </div>
-        <p>{raceParagraph}</p>
-        <TextInputComponent />
+        <TextInputComponent raceParagraphArray={raceParagraphArray}/>
       </div>
     </div>
   );
