766b9c9af91f59b9fe44c4dcbf8dd57396863dce
DanielGroelle <DanielGroelle@protonmail.com>
2024-01-24 21:58:26 -0500

    Update favicon, make some changes to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import TypingJerboaLogo from "../images/TypingJerboaLogoWhiteKB.png";
+
 export default function About() {
   return (
     <div>
@@ -5,7 +9,7 @@ export default function About() {
       <br/>
       <p>TypingJerboa is a website built for the purpose of teaching others how to type in a language that uses a unique script. Currently it supports Latin English and Russian Cyrillic.</p>
       <br/>
-      <p>There are two main modes offered in TypingJerboa: Learn, and Race.</p>
+      <p>There are two main modes offered in TypingJerboa: <Link href="/learn" className="underline">Learn</Link>, and <Link href="/race" className="underline">Race</Link>.</p>
       <br/>
       <p>The Learn section focuses on teaching a user where the characters for the selected language are located on the keyboard over many practice levels. These build on each other so that by the end, they&apos;ll have the muscle memory to type just about anything in the selected language.</p>
       <br/>
@@ -15,10 +19,13 @@ export default function About() {
       <br/>
       <p>If you&apos;d like to keep track of your progress, it is possible and encouraged to register a completely free account, with no email necessary.</p>
       <br/>
-        <a target="_blank" href="https://github.com/DanielGroelle/TypingJerboa" className="underline flex">
-          <img className="bg-white mr-1" width="24" alt="GitHub Invertocat Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/256px-GitHub_Invertocat_Logo.svg.png"></img>
-          TypingJerboa GitHub
-        </a>
+      <a target="_blank" href="https://github.com/DanielGroelle/TypingJerboa" className="underline flex">
+        <img className="bg-white mr-1" width="24" alt="GitHub Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/256px-GitHub_Invertocat_Logo.svg.png"></img>
+        TypingJerboa GitHub
+      </a>
+
+      <Image src={TypingJerboaLogo} alt=""></Image>
+      <p>The jerboa is the website&apos;s mascot. Learn more <a target="_blank" href="https://en.wikipedia.org/wiki/Jerboa" className="underline">here.</a></p>
     </div>
   );
 }
